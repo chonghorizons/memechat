300ab43ea841d029e0092921aa363a13d8148a55
chonghorizons <chonghorizons@gmail.com>
2020-04-09 13:15:03 -0400

    fixed a bunch of gradle

---
+++ lib/mainEmily.dart
@@ -0,0 +1,290 @@
+// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'type_meme.dart';
+import 'platform_adaptive.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memechat',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  List<ChatMessage> _messages = [];
+  DatabaseReference _messagesReference = FirebaseDatabase.instance.reference();
+  TextEditingController _textController = TextEditingController();
+  bool _isComposing = false;
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  var fireBaseSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.signInSilently();
+    FirebaseAuth.instance.signInAnonymously().then((user) {
+      fireBaseSubscription =
+          _messagesReference.onChildAdded.listen((Event event) {
+        var val = event.snapshot.value;
+        _addMessage(
+            name: val['sender']['name'],
+            senderImageUrl: val['sender']['imageUrl'],
+            text: val['text'],
+            imageUrl: val['imageUrl'],
+            textOverlay: val['textOverlay']);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    fireBaseSubscription.cancel();
+    super.dispose();
+  }
+
+  void _handleMessageChanged(String text) {
+    setState(() {
+      _isComposing = text.length > 0;
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    _googleSignIn.signIn().then((user) {
+      var message = {
+        'sender': {'name': user.displayName, 'imageUrl': user.photoUrl},
+        'text': text,
+      };
+      _messagesReference.push().set(message);
+    });
+    setState(() {
+      _isComposing = false;
+    });
+  }
+
+  void _addMessage(
+      {String name,
+      String text,
+      String imageUrl,
+      String textOverlay,
+      String senderImageUrl}) {
+    var animationController = AnimationController(
+      duration: Duration(milliseconds: 700),
+      vsync: this,
+    );
+    var sender = ChatUser(name: name, imageUrl: senderImageUrl);
+    var message = ChatMessage(
+        sender: sender,
+        text: text,
+        imageUrl: imageUrl,
+        textOverlay: textOverlay,
+        animationController: animationController);
+    setState(() {
+      _messages.insert(0, message);
+    });
+    if (imageUrl != null) {
+      NetworkImage image = NetworkImage(imageUrl);
+      ImageStreamListener isl = ImageStreamListener((_, __) {
+        animationController?.forward();
+      });
+      image
+          .resolve(createLocalImageConfiguration(context))
+          .addListener(isl);
+    } else {
+      animationController?.forward();
+    }
+  }
+
+  Future<Null> _handlePhotoButtonPressed() async {
+    var account = await _googleSignIn.signIn();
+    var imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    var random = Random().nextInt(10000);
+    var ref = FirebaseStorage.instance.ref().child('image_$random.jpg');
+    ref.putFile(imageFile);
+    var textOverlay = await Navigator.push(context, TypeMemeRoute(imageFile));
+    if (textOverlay == null) return;
+    String downloadUrl = await ref.getDownloadURL();
+    var message = {
+      'sender': {'name': account.displayName, 'imageUrl': account.photoUrl},
+      'imageUrl': downloadUrl.toString(),
+      'textOverlay': textOverlay,
+    };
+    _messagesReference.push().set(message);
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: PlatformAdaptiveContainer(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  icon: Icon(Icons.photo),
+                  onPressed: _handlePhotoButtonPressed,
+                ),
+              ),
+              Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  onChanged: _handleMessageChanged,
+                  decoration:
+                      InputDecoration.collapsed(hintText: 'Send a message'),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: PlatformAdaptiveButton(
+                    icon: Icon(Icons.send),
+                    onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null,
+                    child: Text('Send'),
+                  )),
+            ])));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PlatformAdaptiveAppBar(
+          title: Text('Memechat'),
+          platform: Theme.of(context).platform,
+        ),
+        body: Column(children: [
+          Flexible(
+              child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) =>
+                ChatMessageListItem(_messages[index]),
+            itemCount: _messages.length,
+          )),
+          Divider(height: 1.0),
+          Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer()),
+        ]));
+  }
+}
+
+class ChatUser {
+  ChatUser({this.name, this.imageUrl});
+  final String name;
+  final String imageUrl;
+}
+
+class ChatMessage {
+  ChatMessage(
+      {this.sender,
+      this.text,
+      this.imageUrl,
+      this.textOverlay,
+      this.animationController});
+  final ChatUser sender;
+  final String text;
+  final String imageUrl;
+  final String textOverlay;
+  final AnimationController animationController;
+}
+
+class ChatMessageListItem extends StatelessWidget {
+  ChatMessageListItem(this.message);
+
+  final ChatMessage message;
+
+  Widget build(BuildContext context) {
+    return SizeTransition(
+        sizeFactor: CurvedAnimation(
+            parent: message.animationController, curve: Curves.easeOut),
+        axisAlignment: 0.0,
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: CircleAvatar(
+                    backgroundImage: NetworkImage(message.sender.imageUrl)),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(message.sender.name,
+                      style: Theme.of(context).textTheme.subhead),
+                  Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: ChatMessageContent(message)),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ChatMessageContent extends StatelessWidget {
+  ChatMessageContent(this.message);
+
+  final ChatMessage message;
+
+  Widget build(BuildContext context) {
+    if (message.imageUrl != null) {
+      var image = Image.network(message.imageUrl, width: 200.0);
+      if (message.textOverlay == null) {
+        return image;
+      } else {
+        return Stack(
+          alignment: FractionalOffset.topCenter,
+          children: [
+            image,
+            Container(
+                alignment: FractionalOffset.topCenter,
+                width: 200.0,
+                child: Text(message.textOverlay,
+                    style: const TextStyle(
+                        fontFamily: 'Anton',
+                        fontSize: 30.0,
+                        color: Colors.white),
+                    softWrap: true,
+                    textAlign: TextAlign.center)),
+          ],
+        );
+      }
+    } else
+      return Text(message.text);
+  }
+}
